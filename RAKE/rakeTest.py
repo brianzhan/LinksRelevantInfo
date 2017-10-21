6e34603c50fc1d3ce8d1c49fd4364ecd939a3cea
yingdahu <yingdahu2018@u.northwestern.edu>
2017-10-21 14:25:08 -0500

    rake

---
+++ RAKE/rakeTest.py
@@ -0,0 +1,16 @@
+import rake
+import operator
+
+#have the text in the document "test.txt"
+with open('test.txt', 'r') as myfile:
+    text = myfile.read().replace('\n', '')
+
+#using constraint where each keyword appears in text at least twice
+rake_object = rake.Rake("SmartStoplist.txt", 3, 3, 2)
+keywords = rake_object.run(text)
+print(keywords)
+
+#using constraint where each keyword appears in text at least three times
+rake_object = rake.Rake("SmartStoplist.txt", 3, 3, 3)
+keywords = rake_object.run(text)
+print(keywords)
